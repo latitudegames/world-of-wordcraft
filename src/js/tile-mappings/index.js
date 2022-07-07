ddfe19ebf3d26298eb3b7715cfab32e53279f06d
Sean Gillespie <info@spg.business>
2022-07-07 17:17:05 -0400

    Add clouds to tile-mappings index

--- src/js/tile-mappings/index.js
+++ src/js/tile-mappings/index.js
@@ -2,10 +2,13 @@ import ATLANTIS_TILES from "./atlantis-tile-mapping.js";
 import EGYPT_TILES from "./egypt-tile-mapping.js";
 import AZTEC_TILES from "./aztec-tile-mapping.js";
 
+import CLOUDS_TILES from "./clouds-tile-mapping.js";
+
 const TILES = {
   atlantis: ATLANTIS_TILES,
   egypt: EGYPT_TILES,
   aztec: AZTEC_TILES,
+  clouds: CLOUDS_TILES,
 };
 
 export default TILES;
