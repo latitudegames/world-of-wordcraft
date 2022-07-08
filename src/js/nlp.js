6da6235f6c7eba3f3146f504626a9840bbb9c043
Mitchell Gordon <mitchell.gordon95@gmail.com>
2022-07-08 17:42:06 -0400

    Second pass at room type selection

--- src/js/nlp.js
+++ src/js/nlp.js
@@ -1,47 +1,11 @@
-const calibrationVals = [
-  1 / 9.5752592086792,
-  1 / 5.215957156615332,
-  1 / 7.2687482833862305,
-  1 / 9.043849766254425,
-  1 / 4.890042215585709,
-];
+import {clipEmbeds} from './clip-embeds.js'
+
+const dot = (a, b) => a.map((_, i) => a[i] * b[i]).reduce((m, n) => m + n);
+
 const roomTypes = ["egypt", "atlantis", "clouds", "aztec", "darkdimension"];
 export const getRoomType = async (textVal) => {
-  // TODO (mitchg) this is kind of hacky, but we need to know how many tokens the prompt is
-  const prompt = `Room Description: a watery dungeon 
-Room Type: atlantis
-
-Room Description: a sandy tomb
-Room Type: egypt
-
-Room Description: heaven with angels
-Room Type: clouds
-
-Room Description: earthy, jungle temple
-Room Type: aztec
-
-Room Description: murky crystal maze
-Room Type: darkdimension
-
-Room Description: ${textVal}
-Room Type:`;
-  const res = await voyage.getGeneration({ prompt, model: "aid-grande", length: 0 });
-  const promptLen = res.data.prompt.tokens.length;
-  const logprobs = await Promise.all(
-    roomTypes.map(async (room, idx) => {
-      const res = await voyage.getGeneration({
-        prompt: prompt + ` ${room}`,
-        model: "aid-grande",
-        length: 0,
-      });
-      const tokens = res.data.prompt.tokens;
-      console.log(tokens.slice(promptLen));
-      return (
-        tokens.slice(promptLen).reduce((acc, next) => acc + next.generatedToken.logprob, 0) *
-        calibrationVals[idx]
-      );
-    })
-  );
-  console.log(logprobs);
-  return roomTypes[logprobs.indexOf(Math.max(...logprobs))];
+  const res = await voyage.getClipEmbed(textVal);
+  const scores = roomTypes.map((room) => dot(res.data.text_embed, clipEmbeds[room]));
+  const maxIdx = scores.indexOf(Math.max(...scores));
+  return roomTypes[maxIdx];
 };
