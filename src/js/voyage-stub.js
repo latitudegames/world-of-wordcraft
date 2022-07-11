09206ad1ef3ffacb0ef07a6a081f979c3ee53d4d
Sean Gillespie <info@spg.business>
2022-07-11 14:29:03 -0400

    Add ts-ignores and lint voyage-stub.js

--- src/js/voyage-stub.js
+++ src/js/voyage-stub.js
@@ -1,39 +1,40 @@
+// @ts-ignore
 voyage = {
-  getGeneration: async function(options) {
-    const response = await fetch(
-      `https://api.latitude.io/text/completions_v2`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': FIX_ME_API_KEY,
-        },
-        body: JSON.stringify(options),
-      }
-    )
+  getGeneration: async function (options) {
+    const response = await fetch(`https://api.latitude.io/text/completions_v2`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // @ts-ignore
+        "x-api-key": FIX_ME_API_KEY,
+      },
+      body: JSON.stringify(options),
+    });
     if (response.status === 200) {
-      return { data: await response.json()}
+      return { data: await response.json() };
     }
 
-    throw Exception(response.status)
+    // @ts-ignore
+    throw Exception(response.status);
   },
-  getClipEmbed: async function(caption) {
+  getClipEmbed: async function (caption) {
     const response = await fetch(
       `https://api.latitude.io/vendor/coreweave/v1/models/clip-encoder/engines/clip-encoder/completions`,
       {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': FIX_ME_API_KEY,
+          "Content-Type": "application/json",
+          // @ts-ignore
+          "x-api-key": FIX_ME_API_KEY,
         },
-        body: JSON.stringify({text: caption}),
+        body: JSON.stringify({ text: caption }),
       }
-    )
+    );
     if (response.status === 200) {
-      return { data: await response.json()}
+      return { data: await response.json() };
     }
 
-    throw Exception(response.status)
-
-  }
-}
+    // @ts-ignore
+    throw Exception(response.status);
+  },
+};
