662e9d1125dd27a64f16b7d226ab277c3162999f
Sean Gillespie <info@spg.business>
2022-08-25 15:06:25 -0400

    Add debug log for getRoomType while deployed

--- src/js/nlp.js
+++ src/js/nlp.js
@@ -1,10 +1,11 @@
-import {clipEmbeds} from './clip-embeds.js'
+import { clipEmbeds } from "./clip-embeds.js";
 
 const dot = (a, b) => a.map((_, i) => a[i] * b[i]).reduce((m, n) => m + n);
 
 const roomTypes = ["egypt", "atlantis", "clouds", "aztec", "darkdimension"];
 export const getRoomType = async (textVal) => {
   const res = await voyage.getClipEmbed(textVal);
+  console.log("Room Type Response", res);
   const scores = roomTypes.map((room) => dot(res.data.text_embed, clipEmbeds[room]));
   const maxIdx = scores.indexOf(Math.max(...scores));
   return roomTypes[maxIdx];
