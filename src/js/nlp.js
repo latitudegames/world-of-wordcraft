0aa71f335d2a7c42acdabfb08ca223b1f1b6e549
Sean Gillespie <info@spg.business>
2022-08-25 15:09:23 -0400

    Remove incurrate data key from getRoomType voyage response parser

--- src/js/nlp.js
+++ src/js/nlp.js
@@ -5,7 +5,6 @@ const dot = (a, b) => a.map((_, i) => a[i] * b[i]).reduce((m, n) => m + n);
 const roomTypes = ["egypt", "atlantis", "clouds", "aztec", "darkdimension"];
 export const getRoomType = async (textVal) => {
   const res = await voyage.getClipEmbed(textVal);
-  console.log("Room Type Response", res);
   const scores = roomTypes.map((room) => dot(res.text_embed, clipEmbeds[room]));
   const maxIdx = scores.indexOf(Math.max(...scores));
   return roomTypes[maxIdx];
